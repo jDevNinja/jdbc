ae0addcb64e27a0df6d3bbb7598a8ae0018dcd0f
Oleg Primolennyy <Oleg.Primolennyy@x5.ru>
2023-11-10 23:42:53 +0700

    2. establish connection with database

--- src/main/java/org/element/onex/Main.java
+++ src/main/java/org/element/onex/Main.java
@@ -1,7 +1,15 @@
 package org.element.onex;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
+  public static void main(String[] args) throws SQLException {
+    String url = "jdbc:postgresql://localhost:5442/postgres";
+    String username = "user";
+    String password = "pass";
+
+    Connection connection = DriverManager.getConnection(url, username, password);
+  }
 }
