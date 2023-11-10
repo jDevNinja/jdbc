dcdfea38d39e4aaf7feeed4a98e17e7805d804d5
Oleg Primolennyy <Oleg.Primolennyy@x5.ru>
2023-11-11 00:33:39 +0700

    7. execute 'UPDATE' with prepared statement

--- src/main/java/org/element/onex/Main.java
+++ src/main/java/org/element/onex/Main.java
@@ -2,6 +2,8 @@ package org.element.onex;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Random;
@@ -17,20 +19,37 @@ public class Main {
     // SQL statements without parameters are normally executed using Statement objects.
     Statement statement = connection.createStatement();
 
+    System.out.println("==statement.Insert==");
     boolean result =
         statement.execute("INSERT INTO account (balance, full_name) VALUES (25, 'John Black');");
     int updateCount = statement.getUpdateCount();
     System.out.println("Результат выполнения: " + result);
     System.out.println("Количество обновленных записей: " + updateCount);
 
+    System.out.println("==statement.Select==");
     result = statement.execute("SELECT * FROM account");
     updateCount = statement.getUpdateCount();
     System.out.println("Результат выполнения: " + result);
     System.out.println("Количество обновленных записей: " + updateCount);
 
+    System.out.println("==statement.Update==");
     int randomBalance = new Random().nextInt(1000);
     String sql = String.format("UPDATE account SET balance = %d WHERE id = 1;", randomBalance);
     result = statement.execute(sql);
+    updateCount = statement.getUpdateCount();
+    System.out.println("Результат выполнения: " + result);
+    System.out.println("Количество обновленных записей: " + updateCount);
+
+    // Чтобы не хардкодить запрос как в строках 23, 29 и сделать более очевидно чем в строке 36
+    // воспользуемся PreparedStatement
+    System.out.println("==preparedStatement.Update==");
+    String[] names = {"One", "Two", "Three"};
+    int random = new Random().nextInt(3);
+    sql = "UPDATE account SET full_name = ? WHERE id = ?";
+    PreparedStatement preparedStatement = connection.prepareStatement(sql);
+    preparedStatement.setString(1, names[random]);
+    preparedStatement.setInt(2, 2);
+    result = preparedStatement.execute();
     updateCount = statement.getUpdateCount();
     System.out.println("Результат выполнения: " + result);
     System.out.println("Количество обновленных записей: " + updateCount);
