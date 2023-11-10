dab83775cb8dc45ba6f16f9d4eea1811af9377aa
Oleg Primolennyy <Oleg.Primolennyy@x5.ru>
2023-11-11 00:47:21 +0700

    8. add mapping for 'SELECT' statement

--- src/main/java/org/element/onex/Main.java
+++ src/main/java/org/element/onex/Main.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
@@ -32,6 +33,21 @@ public class Main {
     System.out.println("Результат выполнения: " + result);
     System.out.println("Количество обновленных записей: " + updateCount);
 
+    // result == true => есть ResultSet, обработаем его
+    ArrayList<Account> accounts = new ArrayList<>();
+    ResultSet resultSet = statement.getResultSet();
+    // нужно пройтись по всему ResultSet
+    while (resultSet.next()) {
+      // нужно указать название колонки и получить из нее соответсвующее значение
+      int id = resultSet.getInt("id");
+      int balance = resultSet.getInt("balance");
+      String fullName = resultSet.getString("full_name");
+      // создать объект и положить его в list
+      accounts.add(new Account(id, fullName, balance));
+    }
+
+    System.out.println("Все найденные аккаунты: " + accounts);
+
     System.out.println("==statement.Update==");
     int randomBalance = new Random().nextInt(1000);
     String sql = String.format("UPDATE account SET balance = %d WHERE id = 1;", randomBalance);
@@ -40,8 +56,8 @@ public class Main {
     System.out.println("Результат выполнения: " + result);
     System.out.println("Количество обновленных записей: " + updateCount);
 
-    // Чтобы не хардкодить запрос как в строках 23, 29 и сделать более очевидно чем в строке 36
-    // воспользуемся PreparedStatement
+    // Чтобы не хардкодить запрос как в строках выше и сделать более очевидно и оптимизированно чем
+    // в строке со String.format() воспользуемся PreparedStatement
     System.out.println("==preparedStatement.Update==");
     String[] names = {"One", "Two", "Three"};
     int random = new Random().nextInt(3);
