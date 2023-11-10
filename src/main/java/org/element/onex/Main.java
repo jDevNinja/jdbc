9a0e047a758861a80d259213cdb2189adf93f187
Oleg Primolennyy <Oleg.Primolennyy@x5.ru>
2023-11-11 00:00:41 +0700

    4. execute 'SELECT' statement

--- src/main/java/org/element/onex/Main.java
+++ src/main/java/org/element/onex/Main.java
@@ -2,6 +2,7 @@ package org.element.onex;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -19,7 +20,11 @@ public class Main {
     boolean result =
         statement.execute("INSERT INTO account (balance, full_name) VALUES (25, 'John Black');");
     int updateCount = statement.getUpdateCount();
+    System.out.println("Результат выполнения: " + result);
+    System.out.println("Количество обновленных записей: " + updateCount);
 
+    result = statement.execute("SELECT * FROM account");
+    updateCount = statement.getUpdateCount();
     System.out.println("Результат выполнения: " + result);
     System.out.println("Количество обновленных записей: " + updateCount);
   }
